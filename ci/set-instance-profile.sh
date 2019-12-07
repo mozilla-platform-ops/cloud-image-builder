748556e49301507ebe2d917d22bdc9b48b09471d
Rob Thijssen <rthijssen@gmail.com>
2019-12-07 13:15:45 +0200

    script for setting instance profile

--- ci/set-instance-profile.sh
+++ ci/set-instance-profile.sh
@@ -2,6 +2,6 @@
 
 for region in eu-central-1 us-east-1 us-east-2 us-west-1 us-west-2; do
   for instance_id in $(aws ec2 --region ${region} describe-instances --filter Name=tag:Name,Values=relops/win2019 Name=instance-state-name,Values=running --query Reservations[*].Instances[*].InstanceId --output text); do
-    aws ec2 associate-iam-instance-profile --instance-id ${instance_id} --iam-instance-profile Name=windows-ami-builder
+    aws ec2 --region ${region} associate-iam-instance-profile --instance-id ${instance_id} --iam-instance-profile Name=windows-ami-builder
   done
 done
