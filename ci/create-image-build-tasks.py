dbaa039c63102ee2bac175558328a64a8d672cd3
Rob Thijssen <rthijssen@gmail.com>
2019-12-19 11:03:18 +0200

    try auto-decode

--- ci/create-image-build-tasks.py
+++ ci/create-image-build-tasks.py
@@ -58,8 +58,8 @@ def createTask(taskId, taskName, taskDescription, provisioner, workerType, comma
 def imageManifestHasChanged(platform, key):
   currentRevision = os.getenv('TRAVIS_COMMIT')
   lastRevision = json.loads(urllib.request.urlopen('https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.relops.cloud-image-builder.{}.{}.latest/artifacts/public/image-bucket-resource.json'.format(platform, key)).read().decode('utf-8'))['build']['revision']
-  currentManifest = urllib.request.urlopen('https://raw.githubusercontent.com/grenade/cloud-image-builder/{}/config/{}-{}.yaml'.format(currentRevision, key, platform)).read().decode('utf-8')
-  lastManifest = urllib.request.urlopen('https://raw.githubusercontent.com/grenade/cloud-image-builder/{}/config/{}-{}.yaml'.format(lastRevision, key, platform)).read().decode('utf-8')
+  currentManifest = urllib.request.urlopen('https://raw.githubusercontent.com/grenade/cloud-image-builder/{}/config/{}-{}.yaml'.format(currentRevision, key, platform)).read().decode()
+  lastManifest = urllib.request.urlopen('https://raw.githubusercontent.com/grenade/cloud-image-builder/{}/config/{}-{}.yaml'.format(lastRevision, key, platform)).read().decode()
   if currentManifest == lastManifest:
     print('info: no change detected for {}-{} manifest between last image build in revision: {} and current revision: {}'.format(key, platform, lastRevision[0:7], currentRevision[0:7]))
   else:
