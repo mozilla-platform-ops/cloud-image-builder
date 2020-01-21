0c4dbf72e742f48c7a13d19be4c562b3fd8c0ca5
Rob Thijssen <rthijssen@gmail.com>
2020-01-21 18:11:24 +0200

    update image tags

--- ci/tag-machine-images.py
+++ ci/tag-machine-images.py
@@ -1,6 +1,8 @@
+import json
 import os
 import re
 import taskcluster
+import urllib.request
 from azure.common.credentials import ServicePrincipalCredentials
 from azure.mgmt.compute import ComputeManagementClient
 
@@ -28,8 +30,25 @@ pattern = re.compile('^{}-{}-([a-z0-9]{{7}})$'.format(group.replace('rg-', ''), 
 images = [x for x in azureComputeManagementClient.images.list_by_resource_group(group) if pattern.match(x.name)]
 for image in images:
   revision = pattern.search(image.name).group(1)
+  commit = json.loads(urllib.request.urlopen('https://api.github.com/repos/grenade/cloud-image-builder/commits/{}'.format(revision)).read().decode())
+  config = yaml.safe_load(urllib.request.urlopen('https://raw.githubusercontent.com/grenade/cloud-image-builder/{}/config/{}-{}.yaml'.format(revision, key, platform)).read().decode())
   print('image: {}, has revision: {}'.format(image.name, revision))
   if image.tags:
     print(', '.join(['%s:: %s' % (key, value) for (key, value) in image.tags.items()]))
   else:
     print('image has no tags')
+  image.tags = {
+    'diskImageCommitDate': commit['commit']['committer']['date'][0:10],
+    'diskImageCommitTime': commit['commit']['committer']['date'],
+    'diskImageCommitSha': commit['sha'],
+    'diskImageCommitMessage': commit['commit']['message'],
+    'isoName': os.path.basename(config['iso']['source']['key']),
+    'isoIndex': config['iso']['wimindex'],
+    'os': config['image']['os'],
+    'edition': config['image']['edition'],
+    'language': config['image']['language'],
+    'architecture': config['image']['architecture']
+  }
+  azureComputeManagementClient.images.create_or_update(group, image.name, image)
+  print('image tags updated')
+  print(', '.join(['%s:: %s' % (key, value) for (key, value) in image.tags.items()]))
