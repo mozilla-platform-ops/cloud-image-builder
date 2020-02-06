57635f9b119606110c2f767838c1614319ad7269
Rob Thijssen <rthijssen@gmail.com>
2020-02-06 11:09:29 +0200

    treat filename changes as rebuild triggers

--- ci/cib.py
+++ ci/cib.py
@@ -61,27 +61,28 @@ def createTask(queue, taskId, taskName, taskDescription, provisioner, workerType
 
 
 def diskImageManifestHasChanged(platform, key, currentRevision):
-  lastRevision = json.loads(gzip.decompress(urllib.request.urlopen('https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.relops.cloud-image-builder.{}.{}.latest/artifacts/public/image-bucket-resource.json'.format(platform, key)).read()).decode('utf-8-sig'))['build']['revision']
+  try:
+    lastRevision = json.loads(gzip.decompress(urllib.request.urlopen('https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.relops.cloud-image-builder.{}.{}.latest/artifacts/public/image-bucket-resource.json'.format(platform, key)).read()).decode('utf-8-sig'))['build']['revision']
+    currentConfig = yaml.safe_load(urllib.request.urlopen('https://raw.githubusercontent.com/grenade/cloud-image-builder/{}/config/{}.yaml'.format(currentRevision, key)).read().decode())
+    previousConfig = yaml.safe_load(urllib.request.urlopen('https://raw.githubusercontent.com/grenade/cloud-image-builder/{}/config/{}.yaml'.format(lastRevision, key)).read().decode())
+  except:
+    return True
 
   imageConfigUnchanged = True
   isoConfigUnchanged = True
   sharedFilesUnchanged = True
 
-  configFile = '{}-{}'.format(key, platform)
-  currentConfig = yaml.safe_load(urllib.request.urlopen('https://raw.githubusercontent.com/grenade/cloud-image-builder/{}/config/{}.yaml'.format(currentRevision, configFile)).read().decode())
-  previousConfig = yaml.safe_load(urllib.request.urlopen('https://raw.githubusercontent.com/grenade/cloud-image-builder/{}/config/{}.yaml'.format(lastRevision, configFile)).read().decode())
-
   if currentConfig['image'] == previousConfig['image']:
-    print('info: no change detected for image definition in {}.yaml between last image build in revision: {} and current revision: {}'.format(configFile, lastRevision[0:7], currentRevision[0:7]))
+    print('info: no change detected for image definition in {}.yaml between last image build in revision: {} and current revision: {}'.format(key, lastRevision[0:7], currentRevision[0:7]))
   else:
     imageConfigUnchanged = False
-    print('info: change detected for image definition in {}.yaml between last image build in revision: {} and current revision: {}'.format(configFile, lastRevision[0:7], currentRevision[0:7]))
+    print('info: change detected for image definition in {}.yaml between last image build in revision: {} and current revision: {}'.format(key, lastRevision[0:7], currentRevision[0:7]))
 
   if currentConfig['iso'] == previousConfig['iso']:
-    print('info: no change detected for iso definition in {}.yaml between last image build in revision: {} and current revision: {}'.format(configFile, lastRevision[0:7], currentRevision[0:7]))
+    print('info: no change detected for iso definition in {}.yaml between last image build in revision: {} and current revision: {}'.format(key, lastRevision[0:7], currentRevision[0:7]))
   else:
     isoConfigUnchanged = False
-    print('info: change detected for iso definition in {}.yaml between last image build in revision: {} and current revision: {}'.format(configFile, lastRevision[0:7], currentRevision[0:7]))
+    print('info: change detected for iso definition in {}.yaml between last image build in revision: {} and current revision: {}'.format(key, lastRevision[0:7], currentRevision[0:7]))
 
   # todo: parse shared config files for change specific to platform/key
   for sharedFile in ['disable-windows-service', 'drivers', 'packages', 'unattend-commands']:
@@ -97,29 +98,29 @@ def diskImageManifestHasChanged(platform, key, currentRevision):
 
 
 def machineImageManifestHasChanged(platform, key, currentRevision, group):
-  lastRevision = json.loads(gzip.decompress(urllib.request.urlopen('https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.relops.cloud-image-builder.{}.{}.latest/artifacts/public/image-bucket-resource.json'.format(platform, key)).read()).decode('utf-8-sig'))['build']['revision']
+  try:
+    lastRevision = json.loads(gzip.decompress(urllib.request.urlopen('https://firefox-ci-tc.services.mozilla.com/api/index/v1/task/project.relops.cloud-image-builder.{}.{}.latest/artifacts/public/image-bucket-resource.json'.format(platform, key)).read()).decode('utf-8-sig'))['build']['revision']
+    currentConfig = yaml.safe_load(urllib.request.urlopen('https://raw.githubusercontent.com/grenade/cloud-image-builder/{}/config/{}.yaml'.format(currentRevision, key)).read().decode())
+    previousConfig = yaml.safe_load(urllib.request.urlopen('https://raw.githubusercontent.com/grenade/cloud-image-builder/{}/config/{}.yaml'.format(lastRevision, key)).read().decode())
+  except:
+    return True
 
   targetTagsUnchanged = True
-
-  configFile = '{}-{}'.format(key, platform)
-  currentConfig = yaml.safe_load(urllib.request.urlopen('https://raw.githubusercontent.com/grenade/cloud-image-builder/{}/config/{}.yaml'.format(currentRevision, configFile)).read().decode())
-  previousConfig = yaml.safe_load(urllib.request.urlopen('https://raw.githubusercontent.com/grenade/cloud-image-builder/{}/config/{}.yaml'.format(lastRevision, configFile)).read().decode())
-
   currentTargetGroupConfig = next((t for t in currentConfig['target'] if t['group'] == group), None)
   previousTargetGroupConfig = next((t for t in previousConfig['target'] if t['group'] == group), None)
 
   if previousTargetGroupConfig is None and currentTargetGroupConfig is not None:
-    print('info: new target group {} detected, in {}.yaml since last image build in revision: {} and current revision: {}'.format(group, configFile, lastRevision[0:7], currentRevision[0:7]))
+    print('info: new target group {} detected, in {}.yaml since last image build in revision: {} and current revision: {}'.format(group, key, lastRevision[0:7], currentRevision[0:7]))
     return True
 
   for tagKey in ['workerType', 'sourceOrganisation', 'sourceRepository', 'sourceRevision']:
     currentTagValue = next((tag for tag in currentTargetGroupConfig['tag'] if tag['name'] == tagKey), { 'value': '' })['value']
     previousTagValue = next((tag for tag in previousTargetGroupConfig['tag'] if tag['name'] == tagKey), { 'value': '' })['value']
     if currentTagValue == previousTagValue:
-      print('debug: no change detected for tag {}, with value "{}", in target group {}, in {}.yaml between last image build in revision: {} and current revision: {}'.format(tagKey, currentTagValue, group, configFile, lastRevision[0:7], currentRevision[0:7]))
+      print('debug: no change detected for tag {}, with value "{}", in target group {}, in {}.yaml between last image build in revision: {} and current revision: {}'.format(tagKey, currentTagValue, group, key, lastRevision[0:7], currentRevision[0:7]))
     else:
       targetTagsUnchanged = False
-      print('info: change detected for tag {}, with previous value "{}", and new value "{}", in target group {}, in {}.yaml between last image build in revision: {} and current revision: {}'.format(tagKey, previousTagValue, currentTagValue, group, configFile, lastRevision[0:7], currentRevision[0:7]))
+      print('info: change detected for tag {}, with previous value "{}", and new value "{}", in target group {}, in {}.yaml between last image build in revision: {} and current revision: {}'.format(tagKey, previousTagValue, currentTagValue, group, key, lastRevision[0:7], currentRevision[0:7]))
   return not targetTagsUnchanged
 
 
