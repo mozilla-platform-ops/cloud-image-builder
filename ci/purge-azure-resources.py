4e5e7e7f50f378bec90ac42f34b68ed8f6721332
Rob Thijssen <rthijssen@gmail.com>
2020-04-16 11:06:01 +0300

    run azure purging from linux worker

--- ci/purge-azure-resources.py
+++ ci/purge-azure-resources.py
@@ -16,43 +16,23 @@ def get_instance(rg_name, vm_name):
   return computeClient.virtual_machines.instance_view(rg_name, vm_name)
 
 
-def relops_resource_group_filter(rg):
-  return (
-    rg.name.startswith('rg-')
-    and '-us-' in rg.name
-    and (
-      rg.name.endswith('-gecko-1')
-      or rg.name.endswith('-gecko-3')
-      or rg.name.endswith('-gecko-t')
-      or rg.name.endswith('-mpd001-1')
-      or rg.name.endswith('-mpd001-3')
-      or rg.name.endswith('-relops')
+def purge_filter(resource, resource_group_name = None):
+  if resource.__class__.__name__ == 'ResourceGroup':
+    return (
+      resource.name.startswith('rg-')
+      and '-us-' in resource.name
+      and (
+        resource.name.endswith('-gecko-1')
+        or resource.name.endswith('-gecko-3')
+        or resource.name.endswith('-gecko-t')
+        or resource.name.endswith('-mpd001-1')
+        or resource.name.endswith('-mpd001-3')
+        or resource.name.endswith('-relops')
+      )
     )
-  )
-
-
-def purge_filter(resource_type, resource_group, resource):
-  if resource_type == 'virtual machine':
-    return resource.provisioning_state == 'Succeeded' and any(status for status in get_instance(resource_group, resource.name).statuses if status.code == 'PowerState/deallocated')
-  elif resource_type == 'network interface':
-    return resource.virtual_machine is None
-  elif resource_type == 'public ip address':
-    return resource.ip_address is None
-  elif resource_type == 'disk':
-    return resource.disk_state == 'Unattached'
   else:
+    print('no filter mechanism identified for {}'.format(resource.__class__.__name__))
     return False
-
-
-def purge_action(resource_type, resource_group, resource_name):
-  if resource_type == 'virtual machine':
-    computeClient.virtual_machines.delete(resource_group, resource_name)
-  elif resource_type == 'network interface':
-    networkClient.network_interfaces.delete(resource_group, resource_name)
-  elif resource_type == 'public ip address':
-    networkClient.public_ip_addresses.delete(resource_group, resource_name)
-  elif resource_type == 'disk':
-    computeClient.disks.delete(resource_group, resource_name)
 
 
 if 'TASKCLUSTER_PROXY_URL' in os.environ:
@@ -69,45 +49,46 @@ networkClient = NetworkManagementClient(azureCredentials, secret['subscription']
 resourceClient = ResourceManagementClient(azureCredentials, secret['subscription'])
 
 allGroups = list(resourceClient.resource_groups.list())
-targetGroups = sys.argv[1:] if len(sys.argv) > 1 else list(map(lambda x: x.name, filter(relops_resource_group_filter, allGroups)))
+targetGroups = sys.argv[1:] if len(sys.argv) > 1 else list(map(lambda x: x.name, filter(purge_filter, allGroups)))
+resource_descriptors = {
+  'virtual machine': {
+    'filter-descriptor': 'deallocated',
+    'list': computeClient.virtual_machines.list,
+    'purge': computeClient.virtual_machines.delete,
+    'filter': lambda virtual_machine, resource_group_name: virtual_machine.provisioning_state == 'Succeeded' and any(status for status in get_instance(resource_group_name, virtual_machine.name).statuses if status.code == 'PowerState/deallocated')
+  },
+  'network interface': {
+    'filter-descriptor': 'orphaned',
+    'list': networkClient.network_interfaces.list,
+    'purge': networkClient.network_interfaces.delete,
+    'filter': lambda network_interface, resource_group_name: network_interface.virtual_machine is None
+  },
+  'public ip address': {
+    'filter-descriptor': 'orphaned',
+    'list': networkClient.public_ip_addresses.list,
+    'purge': networkClient.public_ip_addresses.delete,
+    'filter': lambda public_ip_address, resource_group_name: public_ip_address.ip_address is None
+  },
+  'disk': {
+    'filter-descriptor': 'orphaned',
+    'list': computeClient.disks.list_by_resource_group,
+    'purge': computeClient.disks.delete,
+    'filter': lambda disk, resource_group_name: disk.disk_state == 'Unattached'
+  }
+}
 
 print('scanning subscription (total resource groups: {}, target resource groups: {}): '.format(len(allGroups), len(targetGroups)))
-
-
 for group in targetGroups:
   print('- scanning resource group {}:'.format(group))
-  resources = [
-    {
-      'type-singular': 'virtual machine',
-      'type-plural': 'virtual machines',
-      'filter-descriptor': 'deallocated',
-      'list': computeClient.virtual_machines.list(group)
-    },
-    {
-      'type-singular': 'network interface',
-      'type-plural': 'network interfaces',
-      'filter-descriptor': 'orphaned',
-      'list': networkClient.network_interfaces.list(group)
-    },
-    {
-      'type-singular': 'public ip address',
-      'type-plural': 'public ip addresses',
-      'filter-descriptor': 'orphaned',
-      'list': networkClient.public_ip_addresses.list(group)
-    },
-    {
-      'type-singular': 'disk',
-      'type-plural': 'disks',
-      'filter-descriptor': 'orphaned',
-      'list': computeClient.disks.list_by_resource_group(group)
-    }
-  ]
-  for resource in resources:
-    all_resources = list(resource['list'])
-    filtered_resources = list(filter(lambda x: purge_filter(resource['type-singular'], group, x), all_resources))
-    print('  - {}:'.format(resource['type-plural']))
+  for resource_type, resource_descriptor in resource_descriptors.items():
+    all_resources = list(resource_descriptor['list'](**{'resource_group_name': group}))
+    filtered_resources = list(filter(lambda x: resource_descriptor['filter'](x, group), all_resources))
+    print('  - {}{}:'.format(resource_type, 'es' if resource_type[-1] == 's' else 's'))
     print('    - total: {}'.format(len(all_resources)))
-    print('    - {}: {}'.format(resource['filter-descriptor'], len(filtered_resources)))
+    print('    - {}: {}'.format(resource_descriptor['filter-descriptor'], len(filtered_resources)))
     for resource_item in filtered_resources:
-      purge_action(resource['type-singular'], group, resource_item.name)
-      print('      - deleted: {}'.format(resource_item.name))
+      try:
+        resource_descriptor['purge'](*[group, resource_item.name])
+        print('      - deleted: {}'.format(resource_item.name))
+      except BaseException as e:
+        print('      - failed to delete: {}. {}'.format(resource_item.name, str(e)))
