70df7ac46f156d3f5898923937995567a0810919
Rob Thijssen <rthijssen@gmail.com>
2020-04-28 20:09:07 +0300

    rebuild ronin azure win10 with tt secret
    include environments: production
    
    include keys: win10-64

--- build-machine-image.ps1
+++ build-machine-image.ps1
@@ -94,11 +94,6 @@ function Invoke-BootstrapExecution {
                   }
                 } else {
                   Write-Output -InputObject ('{0} :: bootstrap execution {1}/{2}, attempt {3}; {4}, using shell: {5}, on: {6}/{7}, did not match: "{8}" in std out' -f $($MyInvocation.MyCommand.Name), $executionNumber, $executionCount, $attemptNumber, $execution.name, $execution.shell, $groupName, $instanceName, $execution.test.std.out.match);
-
-                  # todo: remove this and the subsequent two lines when token debugging is complete
-                  Write-Output -InputObject ('{0} :: bootstrap execution {1}/{2}, attempt {3}; {4}, using shell: {5}, on: {6}/{7}, had the following command script content:' -f $($MyInvocation.MyCommand.Name), $executionNumber, $executionCount, $attemptNumber, $execution.name, $execution.shell, $groupName, $instanceName);
-                  Write-Output -InputObject ($runCommandScriptContent)
-
                   if ($execution.on.failure) {
                     Write-Output -InputObject ('{0} :: bootstrap execution {1}/{2}, attempt {3}; {4}, using shell: {5}, on: {6}/{7}, has triggered failure action: {8}' -f $($MyInvocation.MyCommand.Name), $executionNumber, $executionCount, $attemptNumber, $execution.name, $execution.shell, $groupName, $instanceName, $execution.on.failure);
                     switch ($execution.on.failure) {
