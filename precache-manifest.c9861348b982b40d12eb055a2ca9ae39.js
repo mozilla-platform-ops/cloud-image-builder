90bfa7ca6d7f0ee69e4b3f55474acf476c762f98
Rob Thijssen <rthijssen@gmail.com>
2020-05-06 14:37:07 +0300

    Updates

---
+++ precache-manifest.c9861348b982b40d12eb055a2ca9ae39.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3bd09bff6f4e56f7445dff4bfc7f6f67",
+    "url": "/cloud-image-builder/index.html"
+  },
+  {
+    "revision": "98a65b978016cecc2296",
+    "url": "/cloud-image-builder/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "521afd18672faffc2948",
+    "url": "/cloud-image-builder/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "98a65b978016cecc2296",
+    "url": "/cloud-image-builder/static/js/2.55bf5914.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/cloud-image-builder/static/js/2.55bf5914.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "521afd18672faffc2948",
+    "url": "/cloud-image-builder/static/js/main.81eaf14c.chunk.js"
+  },
+  {
+    "revision": "fdf0f9e27cb63827f85a",
+    "url": "/cloud-image-builder/static/js/runtime-main.17bb4560.js"
+  }
+]);
