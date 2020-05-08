12873b4c1005fe529f0d21fa7f989786d74edfe6
Rob Thijssen <rthijssen@gmail.com>
2020-05-08 09:38:02 +0300

    remove unused commit syntax state parsing

--- src/CommitMessage.js
+++ src/CommitMessage.js
@@ -2,59 +2,6 @@ import React from 'react';
 import Badge from 'react-bootstrap/Badge';
 
 class CommitMessage extends React.Component {
-  state = {
-    isPoolDeploy: false,
-    include: {
-      environments: [],
-      keys: [],
-      pools: [],
-      regions: []
-    },
-    exclude: {
-      environments: [],
-      integrations: [],
-      keys: [],
-      pools: [],
-      regions: []
-    }
-  };
-  componentDidMount() {
-    this.setState(state => ({
-      isPoolDeploy: this.props.message.some(line => line === 'pool-deploy'),
-      include: {
-        environments: [
-          ...(this.props.message.some(line => line.startsWith('include environments: ')) ? this.props.message.find(line => line.startsWith('include environments: ')).replace('include environments: ', '').split(', ') : [])
-        ],
-        keys: [
-          ...(this.props.message.some(line => line.startsWith('include keys: ')) ? this.props.message.find(line => line.startsWith('include keys: ')).replace('include keys: ', '').split(', ') : [])
-        ],
-        pools: [
-          ...(this.props.message.some(line => line.startsWith('include pools: ')) ? this.props.message.find(line => line.startsWith('include pools: ')).replace('include pools: ', '').split(', ') : [])
-        ],
-        regions: [
-          ...(this.props.message.some(line => line.startsWith('include regions: ')) ? this.props.message.find(line => line.startsWith('include regions: ')).replace('include regions: ', '').split(', ') : [])
-        ]
-      },
-      exclude: {
-        environments: [
-          ...(this.props.message.some(line => line.startsWith('exclude environments: ')) ? this.props.message.find(line => line.startsWith('exclude environments: ')).replace('exclude environments: ', '').split(', ') : [])
-        ],
-        integrations: [
-          ...((this.props.message.some(line => line === 'no-ci') || (this.props.message.some(line => line === 'no-taskcluster-ci'))) ? ['taskcluster'] : []),
-          ...((this.props.message.some(line => line === 'no-ci') || (this.props.message.some(line => line === 'no-travis-ci'))) ? ['taskcluster'] : [])
-        ],
-        keys: [
-          ...(this.props.message.some(line => line.startsWith('exclude keys: ')) ? this.props.message.find(line => line.startsWith('exclude keys: ')).replace('exclude keys: ', '').split(', ') : [])
-        ],
-        pools: [
-          ...(this.props.message.some(line => line.startsWith('exclude pools: ')) ? this.props.message.find(line => line.startsWith('exclude pools: ')).replace('exclude pools: ', '').split(', ') : [])
-        ],
-        regions: [
-          ...(this.props.message.some(line => line.startsWith('exclude regions: ')) ? this.props.message.find(line => line.startsWith('exclude regions: ')).replace('exclude regions: ', '').split(', ') : [])
-        ]
-      }
-    }));
-  }
   render() {
     return (
       <div>
