e068964901378f143c73715d1d7a132b3f1ef7c7
Rob Thijssen <rthijssen@gmail.com>
2020-05-12 11:17:42 +0300

    Updates

--- precache-manifest.db281f9d9e7417bd8a671c8d7aa1814b.js
+++ precache-manifest.db281f9d9e7417bd8a671c8d7aa1814b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "7d99d32eb3f838315c71f068e4d7caa4",
+    "revision": "e60848f7f83f4be77790e17538b57310",
     "url": "/cloud-image-builder/index.html"
   },
   {
-    "revision": "6eeb777e791d39014f1f",
+    "revision": "3d79226bcbf765daa079",
     "url": "/cloud-image-builder/static/css/2.de424728.chunk.css"
   },
   {
-    "revision": "d73e3953fc59bdcc9af7",
+    "revision": "f7e8fbc4e11317cd2027",
     "url": "/cloud-image-builder/static/css/main.5ecd60fb.chunk.css"
   },
   {
-    "revision": "6eeb777e791d39014f1f",
-    "url": "/cloud-image-builder/static/js/2.b9e49d87.chunk.js"
+    "revision": "3d79226bcbf765daa079",
+    "url": "/cloud-image-builder/static/js/2.850a99a2.chunk.js"
   },
   {
     "revision": "2db80ee6e43bb57ab10aefd739542bd4",
-    "url": "/cloud-image-builder/static/js/2.b9e49d87.chunk.js.LICENSE.txt"
+    "url": "/cloud-image-builder/static/js/2.850a99a2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "d73e3953fc59bdcc9af7",
-    "url": "/cloud-image-builder/static/js/main.517f562c.chunk.js"
+    "revision": "f7e8fbc4e11317cd2027",
+    "url": "/cloud-image-builder/static/js/main.22c93fc5.chunk.js"
   },
   {
     "revision": "fdf0f9e27cb63827f85a",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/cloud-image-builder/precache-manifest.1c93961bec415b04263db9fdc44a95ba.js"
+  "/cloud-image-builder/precache-manifest.db281f9d9e7417bd8a671c8d7aa1814b.js"
 );
 
 self.addEventListener('message', (event) => {
