a9768aff5512346fc456c27a09a00886faf58bcd
Rob Thijssen <rthijssen@gmail.com>
2021-02-01 12:45:35 +0200

    revert index client to anon
    purge-taskcluster-resources
    
    include pools: gecko-t/win7-32-gpu-azure

--- ci/create-image-build-tasks.py
+++ ci/create-image-build-tasks.py
@@ -20,7 +20,7 @@ taskclusterOptions = { 'rootUrl': os.environ['TASKCLUSTER_PROXY_URL'] }
 
 auth = taskcluster.Auth(taskclusterOptions)
 queue = taskcluster.Queue(taskclusterOptions)
-index = taskcluster.Index(taskclusterOptions)
+index = taskcluster.Index(taskcluster.optionsFromEnvironment())
 secrets = taskcluster.Secrets(taskclusterOptions)
 
 secret = secrets.get('project/relops/image-builder/dev')['secret']
