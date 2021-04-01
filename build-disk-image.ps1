cee17c26deb31069bc1b1deb4d734ff8a2991c1f
Rob Thijssen <rthijssen@gmail.com>
2021-04-01 14:24:57 +0300

    debug azure scopes and permissions
    include pools: gecko-t/win7-32-azure

--- build-disk-image.ps1
+++ build-disk-image.ps1
@@ -36,7 +36,7 @@ foreach ($rm in @(
   },
   @{
     'module' = 'posh-minions-managed';
-    'version' = '0.0.116'
+    'version' = '0.0.117'
   },
   @{
     'module' = 'powershell-yaml';
--- ci/purge-deprecated-azure-resources.ps1
+++ ci/purge-deprecated-azure-resources.ps1
@@ -33,7 +33,7 @@ foreach ($rm in @(
   },
   @{
     'module' = 'posh-minions-managed';
-    'version' = '0.0.116'
+    'version' = '0.0.117'
   },
   @{
     'module' = 'powershell-yaml';
