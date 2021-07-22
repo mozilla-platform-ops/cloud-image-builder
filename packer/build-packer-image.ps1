bc0e3ffebd38ffa4850a9dacaa4e061b8078f93a
markcor <mcornmesser@mozilla.com>
2021-07-22 13:50:29 -0700

    testing gpu image builds
    include pools: gecko-t/win10-64-azure
    
    overwrite-machine-image
    
    include environments: production

--- packer/build-packer-image.ps1
+++ packer/build-packer-image.ps1
@@ -65,10 +65,11 @@ function Build-PackerImage {
      # Though hard coded now it should proablaly be a variable that is passed a parameter to the function. 
      # I am trying to have the script and json template agnostic and all unique values will come from the yaml file
      # The values that are label with markco in the yaml file will be replaced next week
-    
-#     $yaml_data = (Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath 'win10-64_packer.yaml') -Raw | ConvertFrom-Yaml)
-#     $yaml_data = (Get-Content -Path (Join-Path -Path $PSScriptRoot -ChildPath 'win10-64-gpu_packer.yaml') -Raw | ConvertFrom-Yaml)
-      $yaml_data = (Get-Content -Path (Join-Path -Path $PSScriptRoot\config -ChildPath 'win10-64-2004.yaml') -Raw | ConvertFrom-Yaml)
+
+      $yaml_file = 'win10-64-2004-gpu.yaml'
+      #$yaml_file = 'win10-64-2004.yaml'
+
+      $yaml_data = (Get-Content -Path (Join-Path -Path $PSScriptRoot\config -ChildPath $yaml_file) -Raw | ConvertFrom-Yaml)
 
      # Get taskcluster secrets
      $secret = (Invoke-WebRequest -Uri ('{0}/secrets/v1/secret/project/relops/image-builder/dev' -f $env:TASKCLUSTER_PROXY_URL) -UseBasicParsing | ConvertFrom-Json).secret;
