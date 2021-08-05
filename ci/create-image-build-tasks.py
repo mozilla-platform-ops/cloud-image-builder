0325b0314e6f9e969f0b4a1dd24d1990976c2254
markcor <mcornmesser@mozilla.com>
2021-08-05 10:20:35 -0700

    testing builds in Europe
    include pools: gecko-t/win10-64-2004-gpu-test
    
    overwrite-machine-image
    
    include environments: production

--- ci/create-image-build-tasks.py
+++ ci/create-image-build-tasks.py
@@ -211,6 +211,8 @@ for platform in includePlatforms:
         with open(configPath, 'r') as stream:
             config = yaml.safe_load(stream)
             isDiskImageForIncludedPool = any('{}/{}'.format(pool['domain'], pool['variant']) in includePools for pool in config['manager']['pool'])
+            print("INCLUDED?"
+            print(isDiskImageForIncludedPool)
             queueDiskImageBuild = (not poolDeploy) and isDiskImageForIncludedPool and (overwriteDiskImage or diskImageManifestHasChanged(platform, key, commitSha))
             if queueDiskImageBuild:
                 if is_packer:
