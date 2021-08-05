12b8e9de373fc8d9e0f204febbc1f04d0b1ec324
markcor <mcornmesser@mozilla.com>
2021-08-05 12:07:58 -0700

    testing builds in Europe
    overwrite-machine-image
    
    include environments: production
    
    include keys:win10-64-2004-gpu-test

--- ci/create-image-build-tasks.py
+++ ci/create-image-build-tasks.py
@@ -213,7 +213,6 @@ for platform in includePlatforms:
             config = yaml.safe_load(stream)
             isDiskImageForIncludedPool = any('{}/{}'.format(pool['domain'], pool['variant']) in includePools for pool in config['manager']['pool'])
             print("INCLUDED?")
-            print(includepools)
             print(isDiskImageForIncludedPool)
             queueDiskImageBuild = (not poolDeploy) and isDiskImageForIncludedPool and (overwriteDiskImage or diskImageManifestHasChanged(platform, key, commitSha))
             if queueDiskImageBuild:
