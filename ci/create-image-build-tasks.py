e4f8c3bc41c032f5a899f62c1c3fdb62bdb3a145
Mark Cornmesser <mcornmesser@mozilla.com>
2022-11-08 13:30:01 -0800

    Upgrade python on Win 11
    include environments: production
    
    include keys:win2012r2-64-l1-alpha

--- ci/create-image-build-tasks.py
+++ ci/create-image-build-tasks.py
@@ -206,6 +206,8 @@ for KEY in includeKeys:
     'win10-64-2009-alpha',
     'win11-64-2009-alpha',
     'win2022-64-2009-alpha',
+    'win2012r2-64-l1',
+    'win2012r2-64-l1-alpha',
     'win2012r2-64-alpha', 
     'win2012r2-64-next',
     'win2012r2-64-compare-alpha'
